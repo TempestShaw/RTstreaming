42b978a8e2793c443c4cd85de4ff06edf200f8ce
TempestShaw <109138344+TempestShaw@users.noreply.github.com>
2024-01-13 18:15:31 +0800

    Adjust spark stream

--- spark_stream.py
+++ spark_stream.py
@@ -2,102 +2,114 @@ import logging
 
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json
+from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType
 
 
 def create_keyspace(session):
     session.execute("""
-            CREATE KEYSPACE IF NOT EXISTS spark_streams
-            WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
-        """)
+        CREATE KEYSPACE IF NOT EXISTS spark_streams
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
+    """)
 
-    print("Keyspace created!")
+    print("Keyspace created successfully!")
+
+
 def create_table(session):
     session.execute("""
-        CREATE TABLE IF NOT EXISTS spark_streams.created_users (
-            id UUID PRIMARY KEY,
-            first_name TEXT,
-            last_name TEXT,
-            gender TEXT,
-            address TEXT,
-            post_code TEXT,
-            email TEXT,
-            username TEXT,
-            registered_date TEXT,
-            phone TEXT,
-            picture TEXT);
-        """)
+    CREATE TABLE IF NOT EXISTS spark_streams.created_users (
+        id UUID PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        gender TEXT,
+        address TEXT,
+        post_code TEXT,
+        email TEXT,
+        username TEXT,
+        registered_date TEXT,
+        phone TEXT,
+        picture TEXT);
+    """)
 
-    print("Table created!")
+    print("Table created successfully!")
 
 
 def insert_data(session, **kwargs):
-    # insertion here
-    print("Inserting data...")
+    print("inserting data...")
 
-    id = kwargs.get('id')
+    user_id = kwargs.get('id')
     first_name = kwargs.get('first_name')
     last_name = kwargs.get('last_name')
     gender = kwargs.get('gender')
     address = kwargs.get('address')
-    post_code = kwargs.get('post_code')
+    postcode = kwargs.get('post_code')
     email = kwargs.get('email')
     username = kwargs.get('username')
+    dob = kwargs.get('dob')
     registered_date = kwargs.get('registered_date')
     phone = kwargs.get('phone')
     picture = kwargs.get('picture')
 
     try:
         session.execute("""
-            INSERT INTO spark_streams.created_users (id, first_name, last_name, gender, address,
-                post_code, email, username, registered_date, phone, picture)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """,(id, first_name, last_name, gender, address,
-                post_code, email, username, registered_date, phone, picture))
-        logging.info()
+            INSERT INTO spark_streams.created_users(id, first_name, last_name, gender, address, 
+                post_code, email, username, dob, registered_date, phone, picture)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (user_id, first_name, last_name, gender, address,
+              postcode, email, username, dob, registered_date, phone, picture))
+        logging.info(f"Data inserted for {first_name} {last_name}")
+
+    except Exception as e:
+        logging.error(f'could not insert data due to {e}')
+
 
 def create_spark_connection():
     s_conn = None
 
     try:
-        s_conn = SparkSession.builder\
-            .appName('SparkDataStreaming')\
-            .config('spark.jars.packages', 'com.datastax.spark:spark-cassandra-connector_2.13:3.41,'
-                                           'org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1') \
-            .config('spark.cassandra.connect.host', 'localhost') \
+        s_conn = SparkSession.builder \
+            .appName('SparkDataStreaming') \
+            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.4.1,"
+                                           "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
+            .config('spark.cassandra.connection.host', 'localhost') \
             .getOrCreate()
-        s_conn.sparkContext.setLogLevel('ERROR')
-        logging.info('Spark connection created successfully!')
+
+        s_conn.sparkContext.setLogLevel("ERROR")
+        logging.info("Spark connection created successfully!")
     except Exception as e:
         logging.error(f"Couldn't create the spark session due to exception {e}")
 
     return s_conn
+
+
 def connect_to_kafka(spark_conn):
     spark_df = None
     try:
         spark_df = spark_conn.readStream \
-            .format("kafka") \
-            .option("kafka.bootstrap.servers", "localhost:9092") \
-            .option("subscribe", "users_created") \
-            .option("startingOffsets", "earliest")\
+            .format('kafka') \
+            .option('kafka.bootstrap.servers', 'localhost:9092') \
+            .option('subscribe', 'users_created') \
+            .option('startingOffsets', 'earliest') \
             .load()
-        logging.info('initial dataframe created successful')
-
+        logging.info("kafka dataframe created successfully")
     except Exception as e:
-        logging.warning(f"kafka dataframe could not be created because {e}")
-        return None
+        logging.warning(f"kafka dataframe could not be created because: {e}")
+
     return spark_df
+
+
 def create_cassandra_connection():
     try:
-        #connecting to the cassandra cluster
+        # connecting to the cassandra cluster
         cluster = Cluster(['localhost'])
 
         cas_session = cluster.connect()
+
         return cas_session
     except Exception as e:
-        logging.error(f"Couldn't create cassandra connection due to {e}")
+        logging.error(f"Could not create cassandra connection due to {e}")
         return None
+
 
 def create_selection_df_from_kafka(spark_df):
     schema = StructType([
@@ -106,7 +118,7 @@ def create_selection_df_from_kafka(spark_df):
         StructField("last_name", StringType(), False),
         StructField("gender", StringType(), False),
         StructField("address", StringType(), False),
-        StructField("postcode", StringType(), False),
+        StructField("post_code", StringType(), False),
         StructField("email", StringType(), False),
         StructField("username", StringType(), False),
         StructField("registered_date", StringType(), False),
@@ -115,26 +127,32 @@ def create_selection_df_from_kafka(spark_df):
     ])
 
     sel = spark_df.selectExpr("CAST(value AS STRING)") \
-        .select(from_json(col("value"), schema).alias('data')).select("data.*")
+        .select(from_json(col('value'), schema).alias('data')).select("data.*")
     print(sel)
+
     return sel
+
+
 if __name__ == "__main__":
-    #create spark connection
+    # create spark connection
     spark_conn = create_spark_connection()
 
     if spark_conn is not None:
+        # connect to kafka with spark connection
         spark_df = connect_to_kafka(spark_conn)
         selection_df = create_selection_df_from_kafka(spark_df)
         session = create_cassandra_connection()
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        with
+
         if session is not None:
             create_keyspace(session)
             create_table(session)
 
-            streaming_query = (selection_df.writeStream.format('org.apache.spark.sql.cassandra')
-                            .option('checkpointLocation','/tmp/checkpoint')
-                            .option('keyspace', 'spark_streams')
-                            .option('table', 'created_users')
-                            .start())
+            logging.info("Streaming is being started...")
+
+            streaming_query = (selection_df.writeStream.format("org.apache.spark.sql.cassandra")
+                               .option('checkpointLocation', '/tmp/checkpoint')
+                               .option('keyspace', 'spark_streams')
+                               .option('table', 'created_users')
+                               .start())
 
             streaming_query.awaitTermination()
